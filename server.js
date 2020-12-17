ddb59e9c1e3c6cf46f077e3a505aac9d8612c12a
Viktor Malmedal <viktormalmedal@gmail.com>
2020-12-17 16:15:04 +0100

    works for all mondays on one sensor WIP

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import express from "express";
+import express, { response } from "express";
 import morgan from "morgan";
 import { config } from "dotenv";
 import { get } from "axios";
 import cors from "cors";
 import Parser from "./utils/parser";
-import fs from "fs";
+import mondayData from "./misc/rawdata_mondays.json";
 
 config();
 
@@ -15,6 +15,9 @@ const app = express();
  * https://daresay-dev.eu-gb.cf.appdomain.cloud/innovativa/SENSOR/FROM/TO/NO/KEY
  */
 const sensor = "A81758FFFE03BC34";
+const BASEURL = "https://daresay-dev.eu-gb.cf.appdomain.cloud/innovativa";
+const parser = new Parser();
+parser.parseFile(`${__dirname}/placeringar_sensorer.xlsx`);
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -38,5 +41,108 @@ app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
 });
 
-const parser = new Parser();
-parser.parseFile(`${__dirname}/placeringar_sensorer.xlsx`);
+const fetchData = (url) => {
+  return get(url)
+    .then((res) => {
+      return {
+        success: true,
+        data: res.data,
+      };
+    })
+    .catch((e) => {
+      return { sucess: false };
+    });
+};
+
+const getAllSensorData = (urls) => {
+  return Promise.all(urls.map(fetchData));
+};
+
+const getUrl = ({ sensor, from, to, nr }) => {
+  return `${BASEURL}/${sensor}/${from}/${to}/${nr}/${process.env.DARESAY_API_KEY}`;
+};
+
+const getAllUrls = (from, to) => {
+  return parser.sensorInfo.map((sensor) => getUrl({ sensor: sensor.sensorId, from: from, to: to, nr: 1000 }));
+};
+
+// Put this in utils
+const convertTime = (t) => {
+  const z = t.getTimezoneOffset() * 60 * 1000;
+  const tLocal = t - z;
+  const dateLocal = new Date(tLocal);
+  const iso = dateLocal.toISOString();
+  return iso.slice(0, 19);
+};
+
+/**
+ * do while date is less than todays date:
+ *    request data from sensor
+ *    save res in object
+ *    increase start and end date with 7 days
+ *
+ * Theoretically this should work for all days.
+ */
+
+app.get("/mondays", async (_, res) => {
+  const start = new Date("2020-01-06T07:00");
+  const end = new Date("2020-01-06T23:00");
+  const today = new Date();
+  const output = [];
+  let i = 0;
+  do {
+    const r = getAllUrls(convertTime(start), convertTime(end));
+    try {
+      const data = await fetchData(r[0]);
+      const parsedData = parser.parseDay(data);
+      output.push({
+        startTime: convertTime(start),
+        endTime: convertTime(end),
+        sensor: parser.sensorInfo[0],
+        data: { day: "monday", times: parsedData },
+      });
+      start.setDate(start.getDate() + 7);
+      end.setDate(end.getDate() + 7);
+      console.log(`iterations: ${i++}`);
+    } catch (e) {
+      console.log(e);
+    }
+  } while (start < today);
+  return res.json(output);
+});
+
+// TODO: Move to utils
+//
+const normalize = (val, max, min) => {
+  if (max - min === 0) return 1;
+  return ((val - min) / (max - min)) * 10;
+};
+
+app.get("/data", (_, res) => {
+  const averageTimes = {};
+  mondayData.forEach((data) => {
+    Object.keys(data.data.times).map((key) => {
+      if (!(key in averageTimes)) {
+        averageTimes[key] = 0;
+      }
+      averageTimes[key] += parseInt(data.data.times[key], 10);
+    });
+  });
+
+  const sumTimes = averageTimes;
+  const normalizedTimes = {};
+  let max = 0,
+    min = Infinity;
+
+  Object.keys(averageTimes).map((key) => {
+    averageTimes[key] /= mondayData.length;
+    if (averageTimes[key] > max) max = averageTimes[key];
+    if (averageTimes[key] < min) min = averageTimes[key];
+  });
+
+  Object.keys(averageTimes).map((key) => {
+    normalizedTimes[key] = normalize(sumTimes[key], max, min).toFixed(2);
+  });
+
+  res.json({ day: "mondays", sumTimes: sumTimes, averageTimes: averageTimes, normalized: normalizedTimes });
+});
--- utils/parser.js
+++ utils/parser.js
@@ -1,28 +1,61 @@
 import xlsx from "node-xlsx";
 
+const places = {
+  nv: "Naturvetarhuset",
+  sv: "Sammhällsvetarhuset",
+  li: "Lindellhallen",
+};
+
 class Parser {
   constructor() {
     this.placements = null;
     this.sensorInfo = null;
   }
 
-  parsePlacements(placements) {
-    for (let i = 0; i < 2; i++) {
-      console.log(placements.data[i]);
-    }
+  parseDay({ data }) {
+    const timeMap = {};
+    data.map((el) => {
+      const h = new Date(el.time).getHours();
+      if (!(h in timeMap)) {
+        timeMap[h] = [];
+      }
+      timeMap[h].push(el.dd.pir);
+    });
+
+    for (const [key, value] of Object.entries(timeMap))
+      timeMap[key] = (value.reduce((a, b) => a + b) / value.length).toFixed(2);
+
+    return timeMap;
+  }
+
+  getPlaceNameAndFloor(data) {
+    const floor = data.match(/\d+/);
+    const key = data.match(/\w{2}/)[0].toLowerCase();
+
+    let info = {
+      place: places[key],
+    };
+
+    if (key !== "li") info = { ...info, floor: parseInt(floor, 10) };
+
+    return info;
   }
 
   parseSensorinfo(sensorInfo) {
-    for (let i = 1; i < 2; i++) {
-      //console.log(sensorInfo.data[i]);
+    const sensors = [];
+    for (let i = 1; i < sensorInfo.data.length; i++) {
+      if (sensorInfo.data[i][4] === "ERS") {
+        const info = this.getPlaceNameAndFloor(sensorInfo.data[i][5]);
+        const sensorId = sensorInfo.data[i][7];
+        sensors.push({ info: info, sensorId: sensorId });
+      }
     }
+    this.sensorInfo = sensors;
   }
 
   parseFile(filename) {
     const workSheetsFromFile = xlsx.parse(filename);
-    const placements = workSheetsFromFile[0];
     const sensorInfo = workSheetsFromFile[2];
-    this.parsePlacements(placements);
     this.parseSensorinfo(sensorInfo);
   }
 }
