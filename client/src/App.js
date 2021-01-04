f049ac06e60a3e129fce4ed395fb5524470c46c4
Alice <oi17alm@cs.umu.se>
2021-01-04 09:57:18 +0100

    alice

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 
 import React from 'react';
 
-import startPage from './components/startPage/startPage';
+import Question from './components/quiz/Question';
 //import onBoarding2 from './components/onBoarding2/onBoarding2';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => (
   <Router>
-      <Route path="/" exact component={startPage}/>
+      <Route path="/" exact component={Question}/>
   </Router>
 );
 
