6c64fd096f93cbe17c277a9cf912989c807dad51
Evelina Malmqvist <id16emt@cs.umu.se>
2021-01-05 12:39:30 +0100

    started with dropdowns

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 
 import Question from './components/quiz/Question';
+import LandingPage from './components/landingPage/landingPage';
 //import onBoarding2 from './components/onBoarding2/onBoarding2';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => (
   <Router>
-      <Route path="/" exact component={Question}/>
+      {/* <Route path="/" exact component={Question}/> */}
+      <Route path="/" exact component={LandingPage}/>
   </Router>
 );
 
